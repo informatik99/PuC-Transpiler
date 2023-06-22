99c0e7d6d839a3f04407e77ba1f175d247b73dc8
justin-swm <justin_dax@online.de>
2023-06-22 18:10:17 +0200

    fixed problem of formatting
    now th code works well

--- javascript.js
+++ javascript.js
@@ -1,1 +1,14 @@
-switch (5) {case 6 : "ungenuegend" break; case 5 : "mangelhaft" break; case 4 : "ausreichend" break; case 3 : "befridigend" break; case 2 : "Gut" break; case 1 : "Sehr Gut" break;}
+switch (5) {
+case 6 : "ungenuegend"
+break;
+case 5 : "mangelhaft"
+break;
+case 4 : "ausreichend"
+break;
+case 3 : "befridigend"
+break;
+case 2 : "Gut"
+break;
+case 1 : "Sehr gut"
+break;
+}
