56f3c5fa06b1fa825eb1be067808a73fde607b4a
justin-swm <justin_dax@online.de>
2023-06-22 16:19:48 +0200

    Update javascript.js
    tested case to switch
    
    problem bei der einrückung noch da

--- javascript.js
+++ javascript.js
@@ -1,9 +1,1 @@
-var fun =  (x) => {
- return (y) => {
- return x
-}
-}
-var name = "Hallo" + "Rene"
-var schwimmer = "hallo" + "Justin"
-var wahr = true
-console.log(fun(5)(11))
+switch (5) {case 6 : "ungenuegend" break; case 5 : "mangelhaft" break; case 4 : "ausreichend" break; case 3 : "befridigend" break; case 2 : "Gut" break; case 1 : "Sehr Gut" break;}
