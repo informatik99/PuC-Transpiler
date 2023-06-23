e6364b52ceea8e6aab146c06ef6e621355d857ca
renehaar <rene.haar@smail.th-koeln.de>
2023-06-23 09:34:24 +0200

    bla

---
+++ test.js
@@ -0,0 +1,9 @@
+var fun = (x) => {
+    return (y) => {
+    return x
+    }
+    }
+    var name = "Hallo" + "Rene"
+    var schwimmer = "hallo" + "Justin"
+    var wahr = true
+    console.log(fun (5) (9))
