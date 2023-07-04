5c5cfb3d3596c72345b36ea342285f20fd17842e
justin-swm <justin_dax@online.de>
2023-07-04 23:47:13 +0200

    def mit 2 parametern funktuniert bei meinem compiler (online))
    https://www.programiz.com/javascript/online-compiler/

--- javascript.js
+++ javascript.js
@@ -1,9 +1,17 @@
-var fun =  (x) => {
- return (y) => {
- return x
+function fun (x) {
+if (x == x) {
+return "x"
+} else {
+return "y"
 }
+}
+function funu (s, j) {
+return (() => {
+console.log (j + " " + s)
+return j + " " + s
+}) ()
 }
 var name = "Hallo" + "Rene"
-var schwimmer = "hallo" + "Justin"
+var schwimmer = name + name
 var wahr = true
-console.log(fun(5)(11))
+funu ("Hallo Text", "jj")
