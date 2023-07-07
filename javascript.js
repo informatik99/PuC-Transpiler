d2d012e83d9d503bf97da0286e01d2d9884ed32a
renehaar <rene.haar@smail.th-koeln.de>
2023-07-07 09:49:45 +0200

    javascript

--- javascript.js
+++ javascript.js
@@ -1,9 +1,17 @@
-var fun =  (x) => {
- return (y) => {
- return x
+function fun (x) {
+if (x == x) {
+return "x"
+} else {
+return "y"
 }
+}
+function funu (s) {
+return (() => {
+console.log ("jdksfjkld")
+return "jdksfjkld"
+}) ()
 }
 var name = "Hallo" + "Rene"
-var schwimmer = "hallo" + "Justin"
+var schwimmer = name + name
 var wahr = true
-console.log(fun(5)(11))
+funu ("Hallo Text")
